c3c61ae9d0a28f7fd7db9f6650c37d719ef4de25
Reddy-madhava <169072149+Reddy-madhava@users.noreply.github.com>
2024-05-07 18:42:36 +0530

    Update UppercaseToLowercase.java

--- UppercaseToLowercase.java
+++ UppercaseToLowercase.java
@@ -1,20 +1,145 @@
 package Testing;
 
-public class UppercaseToLowercase {
-    public static char toLowercase(char str) {
-        if(str >= 65 && str <= 90) {
-            return (char)(str + 32);
-        }
-        else if(str >= 97 && str <= 122) {
-            return (char)(str - 32);
-        }
-        else{
-            return str;
-
-        }
-    }
-
+public class ConvertCharacter {
     public static void main(String[] args) {
-        System.out.println(toLowercase('a'));
+//        System.out.println(toUppercase(""));
+        System.out.println(tc1singleCharacter());
+        System.out.println(tc2checkNull());
+        System.out.println(tc3multipleCharacters());
+        System.out.println(tc4checkSpaces());
+        System.out.println(tc5checkNums());
+        System.out.println(tc6charandnum());
+        System.out.println(tc7allUppercases());
+        System.out.println(tc8checkEmpty());
+        System.out.println(tc9allLowercases());
+        System.out.println(tc10specialCharacters());
     }
+
+    public static String toUppercase(String str) {
+        if (str != null) {
+            String result = "";
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) >= 97 && str.charAt(i) <= 122) {
+                    result += (char) (str.charAt(i) - 32);
+                } else {
+                    result += str.charAt(i);
+                }
+            }
+            return result;
+        }else {
+            return "null";
+        }
+    }
+
+    public static boolean tc1singleCharacter(){
+        String input = "a";
+        String expected = "A";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static boolean tc2checkNull(){
+        String input = null;
+        String expected = "null";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+    public static boolean tc3multipleCharacters(){
+        String input = "maddyAB";
+        String expected = "MADDYAB";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static boolean tc4checkSpaces(){
+        String input = "   ";
+        String expected = "   ";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static boolean tc5checkNums(){
+        String input = "1234";
+        String expected = "1234";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static boolean tc6charandnum(){
+        String input = "abc12";
+        String expected = "ABC12";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+    public static boolean tc7allUppercases(){
+        String input = "ABC";
+        String expected = "ABC";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static boolean tc8checkEmpty(){
+        String input = "";
+        String expected = "";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static boolean tc9allLowercases(){
+        String input = "maddy";
+        String expected = "MADDY";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static boolean tc10specialCharacters(){
+        String input = "@!#";
+        String expected = "@!#";
+        String res=toUppercase(input);
+        if(res.equals(expected)){
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+
+
+
 }
